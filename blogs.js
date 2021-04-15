44b4758d7073adf09ed8f31e5fc732e54c87079a
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-04-15 13:08:07 +0100

    Add date and time field
    Closes #13

--- blogs.js
+++ blogs.js
@@ -1,7 +1,7 @@
 let blogs = {
     message1: {
     author: "Unknown Genius",
-    time: "14:26", 
+    date: "Thu 15 April 2021 14:26pm",
     message: "A blog is a blog. Choose your blog post", 
     }
 };
--- server.js
+++ server.js
@@ -8,73 +8,81 @@ server.use(staticHandler);
 let blogCount = 1;
 
 server.get("/", (request, response) => {
-    let messages = "";
-    let blogIds = Object.keys(blogs);
-    for (let i = 0; i < blogIds.length; i++) {
-      messages += `<li>
-      <span>${blogs[blogIds[i]].author} –</span>
-      <span>${blogs[blogIds[i]].message}</span>
-      <form action="/delete-blog" method="POST" style="display: inline;">
-        <button name="name" value="${blogIds[i]}" aria-label="Delete ${blogs[blogIds[i]].message}">
-          &times;
-        </button>
-      </form>
-    </li>`;
-    }
+  let messages = "";
+  let blogIds = Object.keys(blogs);
+  for (let i = 0; i < blogIds.length; i++) {
+    messages += `<li>
+    <span>${blogs[blogIds[i]].author} –</span>
+    <span>${blogs[blogIds[i]].message}</span>
+    <span>${blogs[blogIds[i]].date}</span>
+    <form action="/delete-blog" method="POST" style="display: inline;">
+      <button name="name" value="${blogIds[i]}" aria-label="Delete ${blogs[blogIds[i]].message}">
+        &times;
+      </button>
+    </form>
+  </li>`;
+  }
   
-    const html = `
-    <!DOCTYPE html>
-    <html lang="en">
+  const html = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Twaddle</title>
+  </head>
+  <body>
+    <header>
+    <h1><img src='twaddle2.png' alt='logo'></h1>
+    </header>
+    <main>
+    <ul>${messages}</ul>
+    <a href="/add-blog">Write post +</a>
+    </main>
+  </body>
+  </html>
+  `;
+
+  response.send(html);
+  });
+
+server.get("/add-blog", (request, response) => {
+  const html = `
+  <!doctype html>
+  <html>
     <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="description" content="">
-      <link rel="stylesheet" type="text/css" href="style.css">
-      <title>Twaddle</title>
+      <meta charset="utf-8">
+      <title>Twadd away!</title>
     </head>
     <body>
-      <header>
-      <h1><img src='twaddle2.png' alt='logo'></h1>
-      </header>
-      <main>
-      <ul>${messages}</ul>
-      <a href="/add-blog">Write post +</a>
-      </main>
+      <h1>Create a blog post</h1>
+      <form id='blog-entry' method="POST">
+        <label for="author">Blogger name</label>
+        <input id="author" name="author">
+        <label for="message">Message</label>
+        <input id="message" name="message">
+        <button>Submit</button>
+      </form>
     </body>
-    </html>
-    `;
-    response.send(html);
-  });
+  </html>
+  `;
+  response.send(html);
+});
 
-  server.get("/add-blog", (request, response) => {
-    const html = `
-    <!doctype html>
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>Twadd away!</title>
-      </head>
-      <body>
-        <h1>Create a blog post</h1>
-        <form id='blog-entry' method="POST">
-          <label for="author">Blogger name</label>
-          <input id="author" name="author">
-          <label for="message">Message</label>
-          <input id="message" name="message">
-          <button>Submit</button>
-        </form>
-      </body>
-    </html>
-    `;
-    response.send(html);
-  });
-
-  const bodyParser = express.urlencoded({ extended: false });
+const bodyParser = express.urlencoded({ extended: false });
 
 server.post("/add-blog", bodyParser, (request, response) => {
   blogCount++;
   const newBlog = request.body;
-  const author = newBlog.author;
+
+  const d = new Date();
+  const time = d.toTimeString().slice(0,5); // e.g. 12.42
+  const date = d.toDateString(); // e.g. Thu 15 April 2021
+  const wholeDate = "- " + date + " " + time;
+
+  newBlog["date"] = wholeDate;
   blogs[`message${blogCount}`] = newBlog;
   response.redirect("/");
 });
@@ -86,7 +94,7 @@ server.post("/delete-blog", bodyParser, (request, response) => {
   response.redirect("/");
 });
 
-
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`)});
+  console.log(`Listening on http://localhost:${PORT}`)
+});
