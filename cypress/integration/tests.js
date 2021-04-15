b160ab6d359ec9b540d500882eb61d1e2ec56ac0
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2021-04-15 11:54:34 +0100

    Write add and delete post tests

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -4,5 +4,28 @@ it("can run a test", () => {
 
   it("creates a homepage", () => {
     cy.visit('http://localhost:3000');
-    cy.get('h1').contains('TEST');
+    cy.get('h1');
   });
+
+  it("creates a new blog post", () => {
+    addTestBlog();
+  })
+
+  it("deletes a blog post", () => {
+    deleteBlog();
+  })
+
+
+  function addTestBlog() {
+    cy.visit('http://localhost:3000/add-blog');
+    cy.get('form').find('input[name="message"]').type("hi");
+    cy.get('form').submit();
+    cy.visit('http://localhost:3000');
+    cy.get('ul').contains('hi');
+  }
+
+  function deleteBlog() {
+    cy.visit('http://localhost:3000');
+    cy.get('form').find("button[value='message2']").click()
+    cy.get('ul').contains('hi').should('not.exist');
+  }
